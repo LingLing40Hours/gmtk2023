071dcdce40dbff6d2ebbbb6f87abf1793865844a
Xiangyu Chen <chenxy327@gmail.com>
2023-07-07 12:19:16 -0600

    switch to 480p, fix level changing bug, connect start button

--- Levels/Level0.gd
+++ Levels/Level0.gd
@@ -5,7 +5,13 @@ extends Node2D
 
 
 func _input(event):
-	if event.is_action_pressed("ui_home"):
-		game.change_level_faded(0);
-	elif event.is_action_pressed("ui_restart"):
-		game.change_level_faded(GV.current_level_index);
+	if event.is_action_pressed("home"):
+		#game.change_level_faded(0);
+		pass;
+	elif event.is_action_pressed("restart"):
+		#game.change_level_faded(GV.current_level_index);
+		pass;
+
+
+func _on_texture_button_pressed():
+	game.change_level_faded(1);
---
+++ Objects/Gun.gd
@@ -0,0 +1,7 @@
+class_name Gun
+extends Node2D
+
+
+var damage:float;
+var speed:float;
+

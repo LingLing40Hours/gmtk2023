8fcb3d5bebd5fb2dfd013d5a70ba0071da963f36
Xiangyu Chen <chenxy327@gmail.com>
2023-07-08 19:44:00 -0600

    finish tracking cam; set level bounds to see tracking in action

--- Levels/TrackingCam.gd
+++ Levels/TrackingCam.gd
@@ -12,22 +12,26 @@ var tween;
 var target:Vector2;
 var transitioned = true;
 var TRANSITION_TIME = 1.28;
+var pmin:Vector2;
+var pmax:Vector2;
 
 
 func _ready():
-	position = player.position;
+	pmin = Vector2(level.xmin + GV.RESOLUTION.x/2, level.ymin + GV.RESOLUTION.y/2);
+	pmax = Vector2(level.xmax - GV.RESOLUTION.x/2, level.ymax - GV.RESOLUTION.y/2);
+	position = player.position.clamp(pmin, pmax);
+	
 
 func _process(_delta):
 	var dx = abs(player.position.x - position.x);
 	var dy = abs(player.position.y - position.y);
 	if transitioned and (dx/GV.RESOLUTION.x >= max_rx or dy/GV.RESOLUTION.y >= max_ry):
-		print("HERE")
 		tween = create_tween();
 		tween.set_ease(Tween.EASE_OUT);
 		tween.finished.connect(_on_tween_transitioned);
 		tween.set_process_mode(Tween.TWEEN_PROCESS_IDLE);
 		
-		target = player.position;
+		target = player.position.clamp(pmin, pmax);
 		tween.tween_property(self, "position", target, TRANSITION_TIME).set_trans(Tween.TRANS_QUINT);
 		
 	#position = player.position;
